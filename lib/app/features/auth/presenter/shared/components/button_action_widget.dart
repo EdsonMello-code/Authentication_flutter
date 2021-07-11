7bae5a07a91403fd477e0b366f906eaef36c7c1b
EdsonMello-code <edsonmelo754@gmail.com>
2021-07-11 09:44:32 -0300

    feat: Addded button action widget

---
+++ lib/app/features/auth/presenter/shared/components/button_action_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:authentication/app/features/auth/presenter/shared/colors/colors.dart';
+
+class ButtonActionWidget extends StatelessWidget {
+  final String title;
+
+  final void Function() onPressed;
+
+  const ButtonActionWidget({
+    Key? key,
+    required this.title,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final primaryColor = Theme.of(context).colorScheme.primaryColor;
+
+    return ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(primaryColor),
+          fixedSize: MaterialStateProperty.all(
+            Size(MediaQuery.of(context).size.width, 40),
+          ),
+        ),
+        onPressed: onPressed,
+        child: Text(
+          title,
+          style: TextStyle(fontSize: 20),
+        ));
+  }
+}
