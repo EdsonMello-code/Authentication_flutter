8ccfb0cd795774feb95c6c17e483246530886e85
EdsonMello-code <edsonmelo754@gmail.com>
2021-07-11 09:55:20 -0300

    feat: Added password text field widget

---
+++ lib/app/features/auth/presenter/shared/components/password_text_field_widget.dart
@@ -0,0 +1,37 @@
+import 'package:authentication/app/features/auth/presenter/shared/strings/strings.dart';
+import 'package:flutter/material.dart';
+
+class PasswordTextFieldWidget extends StatefulWidget {
+  final TextEditingController controller;
+  final FocusNode? focusNode;
+
+  final void Function(String?)? onFieldSubmitted;
+  const PasswordTextFieldWidget(
+      {Key? key,
+      required this.controller,
+      this.focusNode,
+      this.onFieldSubmitted})
+      : super(key: key);
+
+  @override
+  _PasswordTextFieldWidgetState createState() =>
+      _PasswordTextFieldWidgetState();
+}
+
+class _PasswordTextFieldWidgetState extends State<PasswordTextFieldWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      // autofocus: true,
+      onFieldSubmitted: widget.onFieldSubmitted,
+      focusNode: widget.focusNode,
+      controller: widget.controller,
+      decoration: InputDecoration(
+          prefixIcon: const Icon(
+            Icons.lock,
+            size: 20,
+          ),
+          labelText: Strings.password),
+    );
+  }
+}
