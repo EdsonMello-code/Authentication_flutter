3eebeae97af1d7003c5f0f5189e5a36bfe6c264d
EdsonMello-code <edsonmelo754@gmail.com>
2021-07-11 09:55:00 -0300

    feat: Added email text field widget

---
+++ lib/app/features/auth/presenter/shared/components/email_text_field_widget.dart
@@ -0,0 +1,44 @@
+import 'package:authentication/app/features/auth/presenter/shared/strings/strings.dart';
+import 'package:flutter/material.dart';
+
+class EmailTextFieldWidget extends StatefulWidget {
+  final TextEditingController controller;
+
+  final FocusNode? focusNode;
+
+  final Function(String)? onFieldSubmitted;
+
+  const EmailTextFieldWidget({
+    Key? key,
+    required this.controller,
+    this.focusNode,
+    this.onFieldSubmitted,
+  }) : super(key: key);
+
+  @override
+  _EmailTextFieldState createState() => _EmailTextFieldState();
+}
+
+class _EmailTextFieldState extends State<EmailTextFieldWidget> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      autofocus: true,
+      onFieldSubmitted: widget.onFieldSubmitted,
+      focusNode: widget.focusNode,
+      controller: widget.controller,
+      decoration: InputDecoration(
+        prefixIcon: const Icon(
+          Icons.alternate_email,
+          size: 20,
+        ),
+        labelText: Strings.email,
+      ),
+    );
+  }
+}
