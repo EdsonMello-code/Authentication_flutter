c00b6f93aea4ba6fed6391455810eaf2419929e4
EdsonMello-code <edsonmelo754@gmail.com>
2021-07-11 10:01:10 -0300

    fix: moved file for other place

--- lib/app/features/shared/colors/colors.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-extension ColorSchemeCustom on ColorScheme {
-  Color get primaryColor => Color(0xFF6558F5);
-}
--- lib/app/features/shared/strings/strings.dart
+++
@@ -1,9 +0,0 @@
-class Strings {
-  static const String signinTitle = 'Login';
-  static const email = 'Email';
-  static const password = 'Password';
-  static const signupTitle = 'Sign up';
-  static const signinLabel = 'Ou login com...';
-  static const signinSublabel = 'Novo no app?';
-  static const signinLink = 'Signup';
-}
