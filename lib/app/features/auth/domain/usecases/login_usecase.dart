2b3c7acd058507f5676337cc96a4335dcd324d94
EdsonMello-code <edsonmelo754@gmail.com>
2021-07-21 13:40:54 -0300

    fix: Removed validate of login to presenter

--- lib/app/features/auth/domain/usecases/login_usecase.dart
+++ lib/app/features/auth/domain/usecases/login_usecase.dart
@@ -15,23 +15,6 @@ class LoginUsecase implements ILoginRepository {
 
   @override
   Future<Either<LoginError, BaseLoginEntity>> call(LoginDTO params) async {
-    final regexEmail = RegExp(r''' 
-    ^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$
-    ''');
-    final regexPassword = RegExp(
-        r'(?=^.{8,}$)(?=.*\d)(?=.*[!@#$%^&*]+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$');
-
-    final isEmailValid = regexEmail.hasMatch(params.email);
-    final isPasswordValid = regexPassword.hasMatch(params.password);
-
-    if (isEmailValid) {
-      return left(LoginError('Email deve ser válido!'));
-    }
-
-    if (isPasswordValid) {
-      return left(LoginError('Password deve ser mais forte!'));
-    }
-
     final user = await authRepository.login(params);
     return user;
   }
